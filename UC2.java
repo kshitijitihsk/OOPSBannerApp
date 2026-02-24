fa4215ffbe132e209398bfae583f4b20d883b687
kshitijitihsk <km7786@srmist.edu.in>
2026-02-24 12:45:35 +0530

    Update UC2.java

--- UC2.java
+++ UC2.java
@@ -1,11 +1,9 @@
 class UC2 {
     public static void main(String args[]){
-        System.out.println("**	**	**	**");
-		System.out.println("*  *	*  *	*  *	*");
-		System.out.println("*  *	*  *	*  *	*");
-		System.out.println("*  *	*  *	**	**");
-		System.out.println("*  *	*  *	*			*");
-		System.out.println("*  *	*  *	*			*");
-		System.out.println("**	**	*		***");
+        System.out.println(" **** **** **** **** ");
+        System.out.println("* * * * * * * *");
+        System.out.println("* * * * **** **** ");
+        System.out.println("* * * * * *");
+        System.out.println(" **** **** * **** ");
     }
 }
